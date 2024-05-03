162a9574d4e07115fa01c9628ab58511d93a1249
FeGlaeser <fernando.glaesersilva@gmail.com>
2024-05-02 23:20:44 -0300

    dia 1 aprendendo poo

--- AprenderPoo/src/App.java
+++ AprenderPoo/src/App.java
@@ -2,6 +2,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-     
+     Celular cel1 = new Celular("Sansung", 2.3f, 500, true);
+
+    cel1.atualizar(3.67f);
+    cel1.ligar();
+
+     cel1.info();
     }
 }
--- AprenderPoo/src/Celular.java
+++ AprenderPoo/src/Celular.java
@@ -1,5 +1,25 @@
 public class Celular {
-    String marca;
-    float versao;
-    int memoria;
+    private String marca;
+    private float versao;
+    private int memoria;
+    private boolean ligado;
+    
+    public Celular(String marca, float versao, int memoria, boolean ligado) {
+        this.marca = marca;
+        this.versao = versao;
+        this.memoria = memoria;
+        this.ligado = ligado;
+    }
+
+    public void ligar() {
+        this.ligado = !ligado;}
+    
+    public void info() {
+        System.out.printf("Marca: %S Versão: %.2f\n", this.marca, this.versao);
+        System.out.printf("Memoria: %d Ligado: %B", this.memoria, this.ligado);
+    }
+
+    public void atualizar(float versaoNova) {
+        this.versao = versaoNova;
+    }
 }
