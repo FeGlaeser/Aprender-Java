e8f5333e0ed923737ccc0b7feadb105ace95ab90
FeGlaeser <fernando.glaesersilva@gmail.com>
2024-05-08 23:40:39 -0300

    Comit 1

---
+++ Exercicio3/src/Personagem.java
@@ -0,0 +1,12 @@
+public class Personagem {
+    private String nome;
+    private String sexo;
+    private Status pontos;
+
+    public void CreateCharacter(String nome, String sexo) {
+        this.nome = nome;
+        this.sexo = sexo;}
+    
+    
+
+}
---
+++ Exercicio3/src/Status.java
@@ -0,0 +1,7 @@
+public class Status {
+    private int dano;
+    private int vida;
+    private int experiencia;
+    private int nivel;}
+
+    public void 
